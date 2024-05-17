018cdf2c7e3b0d82edb56029ffb42107ce64ead9
Trevor Villwock <trevorvillwock@gmail.com>
2024-05-17 10:29:27 -0500

    adds comments

--- generative_music1.py
+++ generative_music1.py
@@ -6,8 +6,18 @@ s.start()
 melody_met = Metro(0.5).play()
 bass_met = Metro(2).play()
 
+# number of half steps above tonic
+# tonic = first note of the scale
+# dorian mode is shown here
 scale = [0, 2, 3, 5, 7, 9, 10, 12]
 
+# amplitude (volume) envelopes
+# these control how the sounds start and stop
+# Attack Decay Sustain Release
+# Attack - how long the sound takes to start (seconds)
+# Decay - how long the sound takes to reach sustain volume from max (seconds)
+# Sustain - volume level when note is held (volume from 0.0-1.0)
+# Release - how long the note takes to fade away
 melody_env = Adsr(0.1, dur=0.25)
 bass_env = Adsr(0.1, release=1, dur=2)
 
@@ -22,8 +32,8 @@ bass_synth = Osc(table=bass_wav, freq=[midiToHz(36), midiToHz(36)], mul=bass_env
 melody_reverb = Freeverb(melody_synth, 2).out()
 
 def play_melody():
-    global playing_motif
     print("play_melody")
+    # random.random() returns value between 0 and 1
     play_note = random.random() * 3
     change_rhythm = random.random() * 4
     melody_synth.setFreq(midiToHz(60 + scale[random.randint(0, 7)]))
@@ -31,7 +41,7 @@ def play_melody():
     if change_rhythm > 2:
         melody_met.setTime(random.choice([0.25, 0.5, 0.75, 1]))
     
-    if play_note > 1:
+    if play_note > 2:
         melody_env.play()
         print("playing melody")
 
