1f6b45829ec54297485e6b770b3d943c166f0adc
Trevor Villwock <trevorvillwock@gmail.com>
2024-05-29 21:43:49 -0700

    adds random articulation changes

--- generative_music1.py
+++ generative_music1.py
@@ -17,11 +17,11 @@ s.start()
 melody_met = Metro(0.5).play()
 bass_met = Metro(2).play()
 
-articulations = {"staccato": Adsr(0.1, dur=0.25),
-                 "legato": Adsr(attack=0.1, decay=0.5, sustain=0.707, release=0.1, dur=0.5) 
-                 }
+# 0 = staccato, 1 = legato
+# articulations = [Adsr(0.1, dur=0.25), Adsr(attack=0.1, dur=0.25)]
+melody_env = Adsr(0.1, dur=0.25)
 
-current_articulation = "legato"
+current_articulation = 0
 
 # print(articulations["staccato"])
 
@@ -38,7 +38,6 @@ scale = [0, 2, 3, 5, 7, 9, 10, 12,
 # Decay - how long the sound takes to reach sustain volume from max (seconds)
 # Sustain - volume level when note is held (volume from 0.0-1.0)
 # Release - how long the note takes to fade away
-melody_env = Adsr(0.1, dur=0.25)
 bass_env = Adsr(0.1, release=1, dur=2)
 
 # using scale degrees
@@ -51,10 +50,9 @@ motif_step = 0
 # Square waveform generator using first 5 odd harmonics
 melody_wav = SquareTable(5)
 bass_wav = SquareTable(5)
-print(melody_env)
 
-melody_synth = Osc(table=melody_wav, freq=[midiToHz(60), midiToHz(60)], mul=articulations[current_articulation])
-harmonizing_synth = Osc(table=melody_wav, freq=[midiToHz(60), midiToHz(60)], mul=articulations[current_articulation])
+melody_synth = Osc(table=melody_wav, freq=[midiToHz(60), midiToHz(60)], mul=melody_env)
+harmonizing_synth = Osc(table=melody_wav, freq=[midiToHz(60), midiToHz(60)], mul=melody_env)
 bass_synth = Osc(table=bass_wav, freq=[midiToHz(36), midiToHz(36)], mul=bass_env).out()
 
 melody_reverb = Freeverb(melody_synth, 2).out()
@@ -64,26 +62,38 @@ def play_melody():
     global playing_motif, motifs, motif_step, motif_num, use_motif
     # print("play_melody")
     play_note = random.random()
+    change_rhythm = random.random()
+    new_rhythm = random.choice([0.25, 0.5, 0.75, 1])
+    
+    if change_rhythm > 0.5:
+        melody_met.setTime(new_rhythm)
+    
+    # 0 = staccato, 1 = legato
+    current_articulation = round(random.random())
+    if current_articulation == 1:
+        # if legato, make duration of envelope same as note length
+        melody_env.setDur(new_rhythm)
+    else:
+        melody_env.setDur(0.25)
+  
+    # melody_synth.setMul(articulations[current_articulation])
+    # harmonizing_synth.setMul(articulations[current_articulation]) 
+    # print(f"current_articulation: {current_articulation}")
     
     if not playing_motif:
         use_motif = random.random()
         motif_num = round(random.random())
     
-    change_rhythm = random.random()
-    
-    if change_rhythm > 0.5:
-        melody_met.setTime(random.choice([0.25, 0.5, 0.75, 1]))
-    
     if use_motif > 0.6:
         playing_motif = True
         
     if playing_motif:
-        print("playing motif")
-        play_note = 3 # make sure no notes of the motif are replaced with rests
+        # print("playing motif")
+        play_note = 1 # make sure no notes of the motif are replaced with rests
         if motif_step < len(motifs[motif_num]):
-            print(f"motif_num: {motif_num}")
-            print(f"motif_step: {motif_step}")
-            print(f"motifs[motif_num][motif_step]: {motifs[motif_num][motif_step]}")
+            # print(f"motif_num: {motif_num}")
+            # print(f"motif_step: {motif_step}")
+            # print(f"motifs[motif_num][motif_step]: {motifs[motif_num][motif_step]}")
             melody_synth.setFreq(midiToHz(60 + scale[motifs[motif_num][motif_step]]))
             # harmonizing_synth.setFreq(5/4 * midiToHz(60 + scale[motifs[motif_num][motif_step]]))
             harmonizing_synth.setFreq(midiToHz(60 + scale[(motifs[motif_num][motif_step] + 2) % 13]))
@@ -98,7 +108,7 @@ def play_melody():
         # harmonizing_synth.setFreq(5/4 * midiToHz(60 + scale[random_degree]))
         
     if play_note > 0.3:
-        articulations[current_articulation].play()
+        melody_env.play()
         print("playing melody")
 
 def play_bass():
